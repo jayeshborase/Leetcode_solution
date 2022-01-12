cf8feeefd8fe948c00c342716e462db57158162f
Jayesh Borase <53811444+jayeshborase@users.noreply.github.com>
2022-01-13 00:03:27 +0530

    Time: 28 ms (25.83%), Space: 17.1 MB (75.46%) - LeetHub

---
+++ 57-insert-interval/57-insert-interval.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& in, vector<int>& newIn) {
+        in.push_back(newIn);
+        
+        sort(in.begin(),in.end());
+        
+        vector<vector<int>> ans;
+        
+        for(int i = 0; i < in.size(); i++){
+            int left = in[i][0];
+            int right = in[i][1];
+            
+            while(i < in.size()-1 && right >= in[i+1][0]){
+                right = max(right,in[i+1][1]);
+                i++;
+            }
+            ans.push_back({left,right});
+        }
+        
+        return ans;
+    }
+};
