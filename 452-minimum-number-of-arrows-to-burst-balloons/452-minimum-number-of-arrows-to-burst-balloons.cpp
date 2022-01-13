b98118210f10d605bbb3d58b4f91e5b45bca666b
Jayesh Borase <53811444+jayeshborase@users.noreply.github.com>
2022-01-13 22:33:41 +0530

    Time: 957 ms (5.09%), Space: 89.7 MB (63.52%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -8,13 +8,11 @@ public:
         int i = 0;
         int count = 0;
         while(i < si){
-            int le = points[i][0];
-            int ri = points[i][1];
+            int end = points[i][1];
             
-            while(i < si-1 && ri >= points[i+1][0]){
-                // only change here 
-                //if(points[i+1][1] < ri)
-                    ri = min(ri,points[i+1][1]);
+            while(i < si-1 && end >= points[i+1][0]){
+                // only change here ri store minimum value
+                end = min(end,points[i+1][1]);
                 i++;
             }
             count++;
