f2cffc38c0150ea4673253bba6e5a5f0186842df
Jayesh Borase <53811444+jayeshborase@users.noreply.github.com>
2022-01-17 23:22:36 +0530

    Time: 4 ms (13.73%), Space: 7 MB (18.34%) - LeetHub

--- 290-word-pattern/290-word-pattern.cpp
+++ 290-word-pattern/290-word-pattern.cpp
@@ -23,10 +23,13 @@ public:
         int j = 0;
         
         while(i < pattern.length() && j < v.size()){
-            if(p.find(pattern[i]) == p.end() && w.find(v[j]) == w.end()){
-                p[pattern[i]] = v[j];
-                w[v[j]] = pattern[i];
-            }else if(p[pattern[i]] != v[j] || w[v[j]] != pattern[i]){
+            string a = s.substr(0,s.find(' '));
+            s = s.substr(s.find(' ')+1,s.length());
+            cout << a << " ";
+            if(p.find(pattern[i]) == p.end() && w.find(a) == w.end()){
+                p[pattern[i]] = a;
+                w[a] = pattern[i];
+            }else if(p[pattern[i]] != a || w[a] != pattern[i]){
                 return false;
             }
             i++,j++;   
