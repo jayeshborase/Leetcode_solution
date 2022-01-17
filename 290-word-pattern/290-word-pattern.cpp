12e5296cb4bef40cd61ade104c5efa8eb5c20bc8
Jayesh Borase <53811444+jayeshborase@users.noreply.github.com>
2022-01-17 23:30:03 +0530

    Time: 4 ms (13.73%), Space: 7.1 MB (18.34%) - LeetHub

--- 290-word-pattern/290-word-pattern.cpp
+++ 290-word-pattern/290-word-pattern.cpp
@@ -3,22 +3,8 @@ public:
     bool wordPattern(string pattern, string s) {
         map<char,string> p;
         map<string,char> w;
-        //vector<string> v;
         s.push_back(' ');
-        // string ans;
-        // int co = 0;
-        // for(int i = 0; i < s.length(); i++){
-        //     if(s[i] == ' '){
-        //         v.push_back(ans);
-        //         co++;
-        //         ans = "";
-        //     }else{
-        //         ans.push_back(s[i]);
-        //     }
-        // }
-        
-        // if(co != pattern.length())
-        //     return false;
+    
         int i = 0;
         int j = 0;
         
