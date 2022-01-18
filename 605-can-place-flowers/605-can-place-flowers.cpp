db81301b446764025d871d9226cca7022a8216f5
Jayesh Borase <53811444+jayeshborase@users.noreply.github.com>
2022-01-18 22:09:51 +0530

    Time: 8 ms (99.17%), Space: 20.3 MB (69.55%) - LeetHub

---
+++ 605-can-place-flowers/605-can-place-flowers.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& f, int n) {
+        
+        int si = f.size();
+        int co = 0;
+        if(si == 1)
+            return (f[0] == 0 && n == 1) || (f[0] == 1 && n == 0) || (f[0] == 0 && n == 0);
+        
+        for(int i = 0; n != 0 && i < si; i++){
+            if(i == 0 ){
+                if(f[i] == 0 && f[i+1] == 0){
+                    //co++;
+                    n--;
+                    f[i] = 1;
+                }
+            }else if(i == si-1){
+                if(f[i] == 0 && f[i-1] == 0){
+                    //co++;
+                    n--;
+                    f[i] = 1;
+                }
+            }else if(f[i] == 0){
+                if(f[i-1] == 0 && f[i+1] == 0){
+                    n--;
+                    f[i] = 1;
+                }
+                    //co++;
+                
+                // f[i-1] = 2;
+                // f[i+1] = 2;
+            }
+        }
+        
+        if(n == 0)
+            return 1;
+        return 0;
+    }
+};
