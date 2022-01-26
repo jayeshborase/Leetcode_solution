0f5ebced4cd86ef87efd5268935efc4758bd21af
Jayesh Borase <53811444+jayeshborase@users.noreply.github.com>
2022-01-26 14:35:55 +0530

    Time: 235 ms (28.21%), Space: 86.8 MB (15.36%) - LeetHub

---
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void solve(TreeNode* r1, vector<int>&v){
+        if(r1 == NULL)
+            return;
+     
+        solve(r1->left,v);
+       
+        v.push_back(r1->val);
+
+        solve(r1->right,v);
+    }
+    vector<int> marge(vector<int> &v1, vector<int> &v2){
+        int siz1 = v1.size();
+        int siz2 = v2.size();
+        int i = 0, j = 0;
+        vector<int> v;
+        
+        while(i < siz1 && j < siz2){
+            if(v1[i] < v2[j])
+                v.push_back(v1[i++]);
+            else
+                v.push_back(v2[j++]);
+        }
+        
+        while(i < siz1){
+            v.push_back(v1[i++]);
+        }
+        
+        while(j < siz2){
+            v.push_back(v2[j++]);
+        }
+        
+        return v;
+    }
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+        vector<int> v1,v2;
+        solve(root1,v1);
+        solve(root2,v2);
+        return marge(v1,v2);
+    }
+};
