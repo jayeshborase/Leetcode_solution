2327eb47a8db5c50ae0f616f557185df67d5d1c5
Jayesh Borase <53811444+jayeshborase@users.noreply.github.com>
2022-02-01 14:56:20 +0530

    Time: 1436 ms (10.61%), Space: 213.6 MB (20.66%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    void dfs(int node, vector<vector<int>> &adj, vector<int> &vist, set<int> &ans){
-        ans.insert(node);
+    void dfs(int node, vector<vector<int>> &adj, vector<int> &vist, int &s, int &d){
+        //ans.insert(node);
         vist[node] = 1;
-        
+        s = (node == s) ? -1 : s;
+        d = (node == d) ? -1 : d;
         for(auto i : adj[node]){
             if(!vist[i])
-                dfs(i,adj,vist,ans);
+                dfs(i,adj,vist,s,d);
         }
     }
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
@@ -19,11 +20,15 @@ public:
         }
         
         for(int i = 0; i < n; i++){
-            set<int> s;
+            //set<int> s;
+            int s = source;
+            int d = destination;
             if(!vist[i]){
-                dfs(i,adj,vist,s);
+                dfs(i,adj,vist,s,d);
             }
-            if(s.find(source) != s.end() && s.find(destination) != s.end())
+            // if(s.find(source) != s.end() && s.find(destination) != s.end())
+            //     return true;
+            if(s == -1 && d == -1)
                 return true;
         }
         
