a8f6f60aba7b738a6ae09b94fd98266da6a94fb5
Jayesh Borase <53811444+jayeshborase@users.noreply.github.com>
2022-02-01 22:12:25 +0530

    Time: 61 ms (5.11%), Space: 14.9 MB (6.85%) - LeetHub

---
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public: 
+    bool cyclebfs(int n, int p, vector<vector<int>> &adj, vector<int> &vist){
+        queue<pair<int,int>> q;
+        q.push({n,p});
+        vist[n] = 1;
+        
+        while(!q.empty()){
+            int node = q.front().first;
+            int prev = q.front().second;
+            q.pop();
+            
+            for(auto i : adj[node]){
+                if(!vist[i]){
+                    q.push({i,node});
+                    vist[i] = 1;
+                }else if(i != prev){
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        vector<vector<int>> adj(edges.size()+1);
+        vector<int> vist(edges.size()+1);
+        
+        for(auto i : edges){
+            fill(begin(vist),end(vist),0);
+            adj[i[0]].push_back(i[1]);
+            adj[i[1]].push_back(i[0]);
+            if(cyclebfs(i[0],-1,adj,vist)) return i;
+        }
+        
+        return {};
+        
+        
+    }
+};
