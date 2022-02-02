b12506a07a83977885bbe7dfb595d00a6b149b01
Jayesh Borase <53811444+jayeshborase@users.noreply.github.com>
2022-02-02 14:48:24 +0530

    Time: 34 ms (44.24%), Space: 13.6 MB (58.62%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -21,15 +21,15 @@ public:
         return true;
     }
     bool isBipartite(vector<vector<int>>& graph) {
-        //vector<vector<int>> adj()
         vector<int> color(graph.size(),-1);
-        cout << graph[0].size() << " ";
+     
         for(int i = 0; i < graph.size(); i++){
             if(color[i] == -1 && graph[i].size() != 0){
                 if(!bfsbipartite(graph,color,i))
                     return false;
             }
         }
+        
         return true;
     }
 };
