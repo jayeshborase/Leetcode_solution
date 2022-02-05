40d1124e2f7ed1c54ac6bf2202ef09bc362e628d
Jayesh Borase <53811444+jayeshborase@users.noreply.github.com>
2022-02-05 22:10:26 +0530

    Time: 318 ms (14.68%), Space: 12.9 MB (99.29%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -10,33 +10,40 @@
  */
 class Solution {
 public:
-    ListNode* merge(ListNode* l2, ListNode* l1){
-        if(l1 == NULL) return l2;
-        if(l2 == NULL) return l1;
+    ListNode* marge(ListNode* l1, ListNode* l2){
+        if(l2 == NULL)
+            return l1;
+        if(l1 == NULL)
+            return l2;
         if(l1->val > l2->val)
             swap(l1,l2);
+        
         ListNode* res = l1;
         while(l1 != NULL && l2 != NULL){
-            ListNode* tem = NULL;
-            while(l1 != NULL && l1->val <= l2->val){
-                tem = l1;
+            ListNode* temp = NULL;
+            while(l1 != NULL && l2!= NULL && l1->val <= l2->val){
+                temp = l1;
                 l1 = l1->next;
             }
-            tem->next = l2;
+            temp->next = l2;
             swap(l1,l2);
         }
         return res;
-    } 
+    }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(lists.size() == 0)
+        if(lists.size() == 0){
             return NULL;
-        else if(lists.size() == 1)
+        }
+        if(lists.size() == 1){
             return lists[0];
+        }
+        
         ListNode* l1 = lists[0];
         for(int i = 1; i < lists.size(); i++){
             ListNode* l2 = lists[i];
-            l1 = merge(l2, l1);
+            l1 = marge(l1,l2);
         }
+        
         return l1;
     }
 };
