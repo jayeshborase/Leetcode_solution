6ef3e7d651755a5bc82340b8f06cfeb35c1a2269
Jayesh Borase <53811444+jayeshborase@users.noreply.github.com>
2022-02-06 23:53:57 +0530

    Time: 4 ms (86.50%), Space: 11 MB (37.19%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
@@ -6,20 +6,31 @@ public:
         }
     }
     int removeDuplicates(vector<int>& nums) {
-        int si = nums.size();
-        int s = 0;
-        for(int i = 0; i < si-2;i++){
-            s = 0;
-            int j = i;
-            while(j+2 < si && nums[i] == nums[j+2]){
-                j++;
-                s++;
-            }
-            if(s > 0)
-                remele(nums,i+2,s);
-            si -= s;
-        }
+//         int si = nums.size();
+//         int s = 0;
+//         for(int i = 0; i < si-2;i++){
+//             s = 0;
+//             int j = i;
+//             while(j+2 < si && nums[i] == nums[j+2]){
+//                 j++;
+//                 s++;
+//             }
+//             if(s > 0)
+//                 remele(nums,i+2,s);
+//             si -= s;
+//         }
         
-        return si;
+//         return si;
+        int len = nums.size();
+        if(len < 3) return len;
+        int i = 1,count = 1;
+        for(int j=1;j<len;j++){
+            if(nums[j] == nums[j-1]) count++;
+            else count = 1;
+            if(count <= 2){
+                nums[i] = nums[j];
+                i++;
+            }
+        }return i;
     }
 };
