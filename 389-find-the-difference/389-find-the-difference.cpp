d5c44fe15e97c61b85a0c74be6abe53abb078614
Jayesh Borase <53811444+jayeshborase@users.noreply.github.com>
2022-02-07 23:54:19 +0530

    Time: 11 ms (6.98%), Space: 6.6 MB (87.58%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        map<char,int> m;
+        sort(t.begin(),t.end());
+        for(auto i : s){
+            m[i]++;
+        }
+        char ch = NULL;
+        for(auto i : t){
+            if(m.find(i) == m.end()){
+                ch = i;
+                cout << i << " ";
+            }else{
+                m[i]--;
+                if(m[i] == 0)
+                    m.erase(i);
+            }
+             
+        }
+        
+        return ch;
+    }
+};
