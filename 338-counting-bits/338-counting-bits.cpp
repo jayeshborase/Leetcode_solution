4651556609b75cadcc155f347a87c0e37d6ba6be
Jayesh Borase <53811444+jayeshborase@users.noreply.github.com>
2022-02-12 23:52:57 +0530

    Time: 11 ms (29.28%), Space: 9 MB (21.64%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
-    int bina(int n){
+    int bina(int n,vector<int> &dp){
         int co = 0;
         while(n != 0){
             if(n&1){
                co++; 
+            }
+            if(dp[n>>1] != -1){
+                dp[n] = co+dp[n>>1];
+                return dp[n];
             }
             n = n>>1;
         }
@@ -12,9 +16,10 @@ public:
     }
     vector<int> countBits(int n) {
         vector<int> v(1,0);
-        
+        vector<int> dp(n+1,-1);
+        dp[0] = 0;
         for(int i = 1; i <= n; i++){
-            v.push_back(bina(i));
+            v.push_back(bina(i,dp));
         }
         return v;
     }
