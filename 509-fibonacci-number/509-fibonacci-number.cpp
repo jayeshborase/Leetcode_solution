b7962b99bda3c10166d2396b1440ca02cc98d260
Jayesh Borase <53811444+jayeshborase@users.noreply.github.com>
2022-02-12 23:35:13 +0530

    Time: 1 ms (55.35%), Space: 6.2 MB (9.37%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
-    int solve(int n){
+    int solve(int n, vector<int> &dp){
         if(n <= 1)
             return n;
         
-        return solve(n-1) + solve(n-2);
+        if(dp[n] != -1)
+            return dp[n];
+        
+        dp[n] = solve(n-1,dp) + solve(n-2,dp);
+        
+        return dp[n];
     }
     int fib(int n) {
-        return solve(n);
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
     }
 };
