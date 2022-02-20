bfd1ef2156b4c6a3fb4dc9b1269953b12e8cfb7b
Jayesh Borase <53811444+jayeshborase@users.noreply.github.com>
2022-02-20 23:30:39 +0530

    Time: 24 ms (79.82%), Space: 11.3 MB (59.43%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -3,21 +3,20 @@ public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
         int ans = 0;
         sort(intervals.begin(),intervals.end());
-        //vector<pair<int,int>> v;
+        
         int left = intervals[0][0];
         int right = intervals[0][1];
+        
         for(int i = 1; i < intervals.size(); i++){
             
             if(intervals[i][0] > left && intervals[i][1] > right){
-              
                 left = intervals[i][0];
-                //i++;
                 ans++;
             }
              right = max(right, intervals[i][1]);
             
         }
         
-        return ans+1;//v.size();
+        return ans+1;
     }
 };
